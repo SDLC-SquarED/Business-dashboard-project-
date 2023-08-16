454702db5977c337ad9beaf6a11e00a554e2b0c5
Ethan Lee <elee@xpanxion.com>
2023-08-16 17:14:53 -0400

    resolve conflicts

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+
 import { LineChart } from "./components/lineChart";
+
+import StockNews from "./components/StockNews";
 
 function App() {
   return (
     <div className="App">
       <div className="text-2xl text-red-300">Hello World</div>
+
       <LineChart />
+
+      <StockNews />
     </div>
   );
 }
