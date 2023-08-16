4c113324a6ebbbc368434fba2d3fefb2868805c9
Ethan Lee <elee@xpanxion.com>
2023-08-16 16:13:50 -0400

    able to fetch data from API

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import { LineChart } from "./components/lineChart";
 
 function App() {
   return (
     <div className="App">
       <div className="text-2xl text-red-300">Hello World</div>
+      <LineChart />
     </div>
   );
 }
