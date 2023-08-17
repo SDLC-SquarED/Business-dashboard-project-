d66fc31e992b5def2246a4b82f166b18327c0a76
Leonard Mercedes <100613411+LMercedes03@users.noreply.github.com>
2023-08-16 21:47:10 -0400

    Rendered stock line chart

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
+import { StockChart } from "./components/StockChart";
 
 function App() {
+  
   return (
     <div className="App">
       <div className="text-2xl text-red-300">Hello World</div>
+      < StockChart />
     </div>
   );
 }
 
 export default App;
-
-//
