24c6e48e5c0f362aa26b7386f40f5e76db10a37b
Ethan Lee <ethanlee.dev1@gmail.com>
2023-08-20 17:46:05 -0400

    final commit:

--- client/src/components/TickerCard.js
+++ client/src/components/TickerCard.js
@@ -1,29 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { HiHeart, HiOutlineBell } from 'react-icons/hi';
-import { FiHelpCircle } from 'react-icons/fi';
+import React, { useEffect, useState } from "react";
+import { HiHeart, HiOutlineBell } from "react-icons/hi";
+import { FiHelpCircle } from "react-icons/fi";
 
 export const TickerCard = ({ selectedTicker }) => {
   const [metaData, setMetaData] = useState({});
   const [stockData, setStockData] = useState([]);
-
 
   useEffect(() => {
     const apiKey = "gPHn8kB1bTYbxwbII0ZUw6KagpfghRCIVfCVJlFs";
     const apiUrl = `https://api.stockdata.org/v1/data/quote?symbols=${selectedTicker}&api_token=${apiKey}`;
 
     const headers = {
-      Authorization: `Bearer ${apiKey}`
+      Authorization: `Bearer ${apiKey}`,
     };
-// this is a test
+    // this is a test
     fetch(apiUrl, { headers })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data);
         setMetaData(data.meta);
         setStockData(data.data);
       })
-      .catch(error => {
-        console.error('Error fetching stock data:', error);
+      .catch((error) => {
+        console.error("Error fetching stock data:", error);
       });
   }, [selectedTicker]);
 
@@ -32,16 +31,14 @@ export const TickerCard = ({ selectedTicker }) => {
   };
 
   return (
-    <div className="flex space-x-4">
-      {stockData.length > 0 ? (
+    <div className="flex space-x-4 justify-center">
+      {stockData && stockData.length > 0 ? (
         stockData.map((stockItem, index) => (
           <div
             key={index}
-            className="bg-gray-200 rounded-lg shadow-md p-4 flex space-x-6 flex-col"
+            className="bg-gray-200 rounded-lg shadow-md p-4 flex space-x-6 flex-col justify-center"
           >
-            <div className="font-bold text-2xl">
-              {stockItem.ticker}
-            </div>
+            <div className="font-bold text-2xl">{stockItem.ticker}</div>
             <div className="text-gray-600 mt-4">
               <div className="flex justify-between">
                 <span>Name:</span>
@@ -83,22 +80,22 @@ export const TickerCard = ({ selectedTicker }) => {
                 <span>{stockItem["52_week_low"]}</span>
               </div>
             </div>
-            <div className="flex items-center mt-2 space-x-4 text-sm">
+            <div className="flex items-center mt-2 space-x-6 text-sm">
               <div
                 className="flex items-center cursor-pointer"
-                onClick={() => handleIconClick('Watchlist')}
+                onClick={() => handleIconClick("Watchlist")}
               >
                 <HiHeart className="text-red-500 text-lg" /> Watchlist
               </div>
               <div
                 className="flex items-center cursor-pointer"
-                onClick={() => handleIconClick('Alert')}
+                onClick={() => handleIconClick("Alert")}
               >
                 <HiOutlineBell className="text-blue-500 text-lg" /> Alert
               </div>
               <div
                 className="flex items-center cursor-pointer"
-                onClick={() => handleIconClick('Help')}
+                onClick={() => handleIconClick("Help")}
               >
                 <FiHelpCircle className="text-green-500 text-lg" /> Help
               </div>
@@ -108,7 +105,6 @@ export const TickerCard = ({ selectedTicker }) => {
       ) : (
         <div className="text-gray-500">Loading stock data...</div>
       )}
-      
     </div>
   );
 };
